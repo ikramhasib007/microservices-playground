345445e27334ffeb9f3977089d6a2f1d73273841
ikramhasib007 <ikramhasib007@gmail.com>
2023-06-25 01:15:58 +0600

    auth dev started

--- ticketing/auth/src/index.ts
+++ ticketing/auth/src/index.ts
@@ -1,9 +1,15 @@
 import express from "express";
 import { json } from "body-parser";
+import { currentUserRouter } from "./routes/current-user";
+import { signinRouter } from "./routes/signin";
+import { signupRouter } from "./routes/signup";
+import { signoutRouter } from "./routes/signout";
 
 const app = express();
 app.use(json());
 
+app.use([currentUserRouter, signinRouter, signupRouter, signoutRouter]);
+
 app.listen(3000, () => {
-  console.log("listening on port 3000!!!!!");
+  console.log("listening on port 3000");
 });
