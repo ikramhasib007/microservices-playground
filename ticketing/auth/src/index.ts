7ab70591db2f12f9f5852bb8cf6f99d993f16128
ikramhasib007 <ikramhasib007@gmail.com>
2023-06-25 03:01:04 +0600

    Response Normalization strategies

--- ticketing/auth/src/index.ts
+++ ticketing/auth/src/index.ts
@@ -1,14 +1,23 @@
 import express from "express";
+import "express-async-errors";
 import { json } from "body-parser";
 import { currentUserRouter } from "./routes/current-user";
 import { signinRouter } from "./routes/signin";
 import { signupRouter } from "./routes/signup";
 import { signoutRouter } from "./routes/signout";
+import { errorHandler } from "./middlewares/error-hadler";
+import { NotFoundError } from "./errors/not-found-error";
 
 const app = express();
 app.use(json());
 
 app.use([currentUserRouter, signinRouter, signupRouter, signoutRouter]);
+
+app.all("*", async (req, res) => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
 
 app.listen(3000, () => {
   console.log("listening on port 3000");
