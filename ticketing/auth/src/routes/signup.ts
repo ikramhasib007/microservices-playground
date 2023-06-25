6964f3faad92ec5806849a7b5b18bccbdadc8fb1
ikramhasib007 <ikramhasib007@gmail.com>
2023-06-25 17:10:18 +0600

    bad request error create

--- ticketing/auth/src/routes/signup.ts
+++ ticketing/auth/src/routes/signup.ts
@@ -1,7 +1,8 @@
 import express, { Request, Response } from "express";
 import { body, validationResult } from "express-validator";
+import { User } from "../models/user";
 import { RequestValidationError } from "../errors/request-validation-error";
-import { DatabaseConnectionError } from "../errors/databse-connection-error";
+import { BadReqeustError } from "../errors/bad-request-error";
 
 const router = express.Router();
 
@@ -22,8 +23,15 @@ router.post(
     }
 
     const { email, password } = req.body;
-    console.log("Create a user...");
-    throw new DatabaseConnectionError();
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      throw new BadReqeustError("Email in use");
+    }
+
+    const user = User.build({ email, password });
+    await user.save();
+    res.status(201).send(user);
   }
 );
 
