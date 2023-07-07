9d9f88f627f2bc54aee0c6af7189c27ede078930
ikramhasib007 <ikramhasib007@gmail.com>
2023-07-08 01:20:13 +0600

    signout tested

---
+++ ticketing/auth/__tests__/integration/routes/signout.test.ts
@@ -0,0 +1,23 @@
+import request from "supertest";
+import { app } from "../../../src/app";
+
+describe("Signout", () => {
+  it("should clears the cookie when signing out", async () => {
+    await request(app)
+      .post("/api/users/signup")
+      .send({
+        email: "test@test.com",
+        password: "password",
+      })
+      .expect(201);
+
+    const response = await request(app)
+      .post("/api/users/signout")
+      .send({})
+      .expect(200);
+
+    expect(response.get("Set-Cookie")[0]).toBe(
+      "session=; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT; httponly"
+    );
+  });
+});
