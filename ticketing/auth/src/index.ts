b29286decc99a2a79d7abf0a4ac4cbf1e1e22b49
Ikram Ud Daula (Contractor) <ikram.daula@greenlight.me>
2023-07-10 14:10:40 +0600

    deployment issues fix

--- ticketing/auth/src/index.ts
+++ ticketing/auth/src/index.ts
@@ -3,13 +3,14 @@ import { app } from "./app";
 
 const start = async () => {
   /**
-   * kubectl create secret generic jwt-secret –from-literal=JWT_KEY=asdf
-   * kubectl create secret generic [secret-name] –from-literal=[key]=[value]
+   * kubectl create secret generic jwt-secret --from-literal=JWT_KEY=FA1F4D77A673182FD463323D25DBD
+   * kubectl create secret generic [secret-name] --from-literal=[key]=[value]
    */
   if (!process.env.JWT_KEY) throw new Error("JWT_KEY must be defined");
 
   try {
     await mongoose.connect("mongodb://auth-mongo-srv:27017/auth", {});
+    console.log("MongoDB connected!");
   } catch (error) {
     console.log(error);
   }
