c3064645a2d556d7b81a1a829421ab078e9e8fee
ikramhasib007 <ikramhasib007@gmail.com>
2023-07-18 00:35:21 +0600

    TTD for new tickets route

---
+++ ticketing/tickets/__tests__/integration/routes/new.test.ts
@@ -0,0 +1,8 @@
+import request from "supertest";
+import { app } from "../../../src/app";
+
+it("has a route handler listening to /api/tickets for post requests", async () => {});
+it("should only be accessed if the user is signed in", async () => {});
+it("should returns an error if an invalid title is provided", async () => {});
+it("should returns an error if an invalid price is provided", async () => {});
+it("should creates a ticket with valid inputs", async () => {});
