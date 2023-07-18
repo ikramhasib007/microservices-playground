2d86e74dc61b2b3b039485c8f9a1d8ac3f4b326e
ikramhasib007 <ikramhasib007@gmail.com>
2023-07-18 20:18:11 +0600

    get routes tested

--- ticketing/tickets/src/routes/show.ts
+++ ticketing/tickets/src/routes/show.ts
@@ -1,12 +1,10 @@
 import express, { Request, Response } from "express";
-import { body } from "express-validator";
-import { NotFoundError, requireAuth, validateRequest } from "@concat7/common";
+import { NotFoundError } from "@concat7/common";
 import { Ticket } from "../models/ticket";
 
 const router = express.Router();
 
 router.get("/api/tickets:id", async (req: Request, res: Response) => {
-  console.log("***** req.params: ", req.params);
   const ticket = await Ticket.findById(req.params.id);
   if (!ticket) {
     throw new NotFoundError();
