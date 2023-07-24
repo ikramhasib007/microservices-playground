7753d9524305e112374fd4e5c2b36ecdd06b5b32
ikramhasib007 <ikramhasib007@gmail.com>
2023-07-25 01:32:00 +0600

    nats clientID added on env

--- ticketing/tickets/src/index.ts
+++ ticketing/tickets/src/index.ts
@@ -12,12 +12,14 @@ const start = async () => {
 
   if (!process.env.NATS_CLUSTERID)
     throw new Error("NATS_CLUSTERID must be defined");
+  if (!process.env.NATS_CLIENTID)
+    throw new Error("NATS_CLIENTID must be defined");
   if (!process.env.NATS_URL) throw new Error("NATS_URL must be defined");
 
   try {
     await natsWrapper.connect(
       process.env.NATS_CLUSTERID,
-      "sdfjsiuoi987",
+      process.env.NATS_CLIENTID,
       process.env.NATS_URL
     );
 
