fee8a5049efd26168d73c86fade9fc7cc6fb359c
ikramhasib007 <ikramhasib007@gmail.com>
2023-07-27 08:38:32 +0600

    orders service model created

--- ticketing/orders/src/models/ticket.ts
+++ ticketing/orders/src/models/ticket.ts
@@ -2,14 +2,12 @@ import mongoose from "mongoose";
 
 interface TicketAttrs {
   title: string;
-  price: number;
-  userId: string;
+  price: string;
 }
 
-interface TicketDoc extends mongoose.Document {
+export interface TicketDoc extends mongoose.Document {
   title: string;
-  price: number;
-  userId: string;
+  price: string;
 }
 
 interface TicketModel extends mongoose.Model<TicketDoc> {
@@ -25,10 +23,7 @@ const ticketSchema = new mongoose.Schema(
     price: {
       type: Number,
       required: true,
-    },
-    userId: {
-      type: String,
-      required: true,
+      min: 0,
     },
   },
   {
