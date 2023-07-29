9c048a9a5bab49edb04e71f07c0ce6e394245be6
ikramhasib007 <ikramhasib007@gmail.com>
2023-07-30 03:14:32 +0600

    orders service listener tested

---
+++ ticketing/orders/src/events/listeners/__tests__/ticket-updated-listener.test.ts
@@ -0,0 +1,72 @@
+import mongoose from "mongoose";
+import { Message } from "node-nats-streaming";
+import { TicketUpdatedEvent } from "@concat7/common";
+import { TicketUpdatedListener } from "../ticket-updated-listener";
+import { natsWrapper } from "../../../nats-wrapper";
+import { Ticket } from "../../../models/ticket";
+
+const setup = async () => {
+  // Create a listener
+  const listener = new TicketUpdatedListener(natsWrapper.client);
+
+  // Create and save a ticket
+  const ticket = Ticket.build({
+    id: new mongoose.Types.ObjectId().toHexString(),
+    title: "new movie (thor)",
+    price: 20,
+  });
+  await ticket.save();
+
+  // Create a fake data object
+  const data: TicketUpdatedEvent["data"] = {
+    id: ticket.id,
+    version: ticket.version + 1,
+    title: "new concert",
+    price: 999,
+    userId: new mongoose.Types.ObjectId().toHexString(),
+  };
+
+  // Create a fake msg object
+  // @ts-ignore
+  const msg: Message = {
+    ack: jest.fn(),
+  };
+
+  // return all of this stuff
+  return { msg, data, ticket, listener };
+};
+
+it("should finds, updates, and saves a ticket", async () => {
+  const { msg, data, ticket, listener } = await setup();
+
+  await listener.onMessage(data, msg);
+
+  const updatedTicket = await Ticket.findById(ticket.id);
+
+  expect(updatedTicket!.title).toEqual(data.title);
+  expect(updatedTicket!.price).toEqual(data.price);
+  expect(updatedTicket!.version).toEqual(data.version);
+});
+
+it("should acks the message", async () => {
+  const { msg, data, listener } = await setup();
+
+  await listener.onMessage(data, msg);
+
+  expect(msg.ack).toHaveBeenCalled();
+  expect(msg.ack).toHaveBeenCalledTimes(1);
+});
+
+it("should'not call ack if the event has a skipped version number", async () => {
+  const { msg, data, listener } = await setup();
+
+  data.version = 10;
+
+  try {
+    await listener.onMessage(data, msg);
+  } catch (error: any) {
+    expect(error.message).toBe("Ticket not found!");
+  }
+
+  expect(msg.ack).not.toHaveBeenCalled();
+});
--- ticketing/orders/src/events/listeners/ticket-updated-listener.ts
+++ ticketing/orders/src/events/listeners/ticket-updated-listener.ts
@@ -9,8 +9,10 @@ export class TicketUpdatedListener extends Listener<TicketUpdatedEvent> {
 
   async onMessage(data: TicketUpdatedEvent["data"], msg: Message) {
     const { id, title, price } = data;
+
     const ticket = await Ticket.findByEvent(data);
     if (!ticket) throw new Error("Ticket not found!");
+
     ticket.set({ title, price });
     await ticket.save();
     msg.ack();
