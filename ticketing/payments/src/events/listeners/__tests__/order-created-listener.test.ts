1e2d2021c7159b361dcf3c8d7072b8188ec3439f
ikramhasib007 <ikramhasib007@gmail.com>
2023-08-06 03:30:12 +0600

    order created events and tested

---
+++ ticketing/payments/src/events/listeners/__tests__/order-created-listener.test.ts
@@ -0,0 +1,79 @@
+import { Message } from "node-nats-streaming";
+import mongoose from "mongoose";
+import { OrderCreatedEvent, OrderStatus } from "@concat7/common";
+import { natsWrapper } from "../../../nats-wrapper";
+import { OrderCreatedListener } from "../order-created-listener";
+import { Order } from "../../../models/order";
+
+const setup = async () => {
+  // create an instance of the listener
+  const listener = new OrderCreatedListener(natsWrapper.client);
+
+  // create a fake data event
+  const data: OrderCreatedEvent["data"] = {
+    id: new mongoose.Types.ObjectId().toHexString(),
+    status: OrderStatus.Created,
+    userId: new mongoose.Types.ObjectId().toHexString(),
+    expiresAt: new Date().toISOString(),
+    version: 0,
+    ticket: {
+      id: new mongoose.Types.ObjectId().toHexString(),
+      price: 20,
+    },
+  };
+  // create a fake message object
+  // @ts-ignore
+  const msg: Message = {
+    ack: jest.fn(),
+  };
+
+  return { listener, data, msg };
+};
+
+it("should replicates the order info", async () => {
+  const { listener, data, msg } = await setup();
+
+  // call the onMessage function with the data object + message object
+  await listener.onMessage(data, msg);
+
+  // write assertions to make sure a ticket was updated with orderId!
+  const orderCreated = await Order.findById(data.id);
+
+  expect(orderCreated).toBeDefined();
+  expect(orderCreated!.id).toBe(data.id);
+});
+
+it("should acks the message", async () => {
+  const { listener, data, msg } = await setup();
+
+  // call the onMessage function with the data object + message object
+  await listener.onMessage(data, msg);
+
+  // write assertions to make sure ack function is called
+  expect(msg.ack).toHaveBeenCalled();
+  expect(msg.ack).toHaveBeenCalledTimes(1);
+});
+
+// it("should'not call ack if the event hasn't found the ticket", async () => {
+//   const { msg, data, listener } = await setup();
+
+//   data.ticket.id = new mongoose.Types.ObjectId().toHexString();
+
+//   await expect(listener.onMessage(data, msg)).rejects.toThrow(
+//     "Ticket not found!"
+//   );
+
+//   expect(msg.ack).not.toHaveBeenCalled();
+// });
+
+// it("should publishes a ticket update event", async () => {
+//   const { msg, data, listener } = await setup();
+
+//   await listener.onMessage(data, msg);
+//   expect(natsWrapper.client.publish).toHaveBeenCalled();
+
+//   const ticketUpdatedData = JSON.parse(
+//     (natsWrapper.client.publish as jest.Mock).mock.calls[0][1]
+//   );
+//   expect(data.id).toEqual(ticketUpdatedData.orderId);
+// });
