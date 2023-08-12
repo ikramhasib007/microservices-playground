a70624654c2d4628b727c6404c317bce86d9f9eb
ikramhasib007 <ikramhasib007@gmail.com>
2023-08-13 00:04:40 +0600

    last minute change

--- ticketing/auth/__tests__/integration/routes/current-user.test.ts
+++ ticketing/auth/__tests__/integration/routes/current-user.test.ts
@@ -18,7 +18,7 @@ describe("Current user", () => {
     const response = await request(app)
       .get("/api/users/current-user")
       .send()
-      .expect(200);
+      .expect(400);
     expect(response.body.currentUser).toBeNull();
   });
 });
